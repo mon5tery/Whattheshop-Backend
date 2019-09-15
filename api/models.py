d370782abb7b5a050c268d78b4132ec143310c7d
Nbalissa <nawaf@fanarakw.com>
2019-09-15 18:36:09 +0300

    create item model

--- api/models.py
+++ api/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Item(models.Model):
+    product_name = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField()
+    category = models.CharField(max_length=15)
+    created_on = models.DateTimeField()
+
+    def __str__(self):
+        return self.product_name
