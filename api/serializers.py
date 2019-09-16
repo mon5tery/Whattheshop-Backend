a68305f451f6738f656cc20829ccab3cca8cc388
ahalrasheed <ahmed.alrasheed88@gmail.com>
2019-09-16 17:42:26 +0300

    serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models improt Item
+from rest_framework_jwt.settings import api_settings
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -14,4 +15,50 @@ class UserCreateSerializer(serializers.ModelSerializer):
         new_user = User(username=username)
         new_user.set_password(password)
         new_user.save()
+
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+		jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+		payload = jwt_payload_handler(new_user)
+		token = jwt_encode_handler(payload)
+
+		validated_data["token"] = token
         return validated_data
+
+class ItemtDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Item
+		fields=[
+			'id',
+			'product_name',
+			'price',
+			'description',
+			'image',
+			'category',
+		]
+
+class UserLoginSerializer(serializers.Serializer):
+	username = serializers.CharField()
+	password = serializers.CharField(write_only=True)
+	token = serializers.CharField(allow_blank=True, read_only=True)
+
+	def validate(self, data):
+		my_username = data.get('username')
+		my_password = data.get('password')
+		try:
+			user_obj = User.objects.get(username=my_username)
+			print(user_obj)
+			jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+			jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+			payload = jwt_payload_handler(user_obj)
+			token = jwt_encode_handler(payload)
+
+			data["token"] = token
+		except:
+			raise serializers.ValidationError("This username does not exist")
+
+		if not user_obj.check_password(my_password):
+			raise serializers.ValidationError("Incorrect username/password combination!")
+
+		return data
--- api/views.py
+++ api/views.py
@@ -1,5 +1,17 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from .serializers import UserCreateSerializer
+from rest_framework.views import APIView
+
+from .models import Item
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
+
+class UserLoginView(APIView):
+	serializer_class = UserLoginSerializer
+
+class ItemDetailAPIView(RetrieveAPIView):
+	queryset = Item.objects.all()
+	serializer_class = ItemDetailSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'item_id'
